eb4d5e4a478419cf5f43fce189be64e90c0ea47a
alexrnguyen <alexander.nguyen@shaw.ca>
2023-08-22 20:30:11 -0600

    Add footer

---
+++ src/ui/footer.js
@@ -0,0 +1,13 @@
+const createFooter = () => {
+  const footerContainer = document.createElement("footer");
+
+  const footerText = document.createElement("p");
+  footerText.id = "footer-text";
+  footerText.innerHTML =
+    "Powered by <a href='https://www.weatherapi.com/'>WeatherAPI.com</a>";
+
+  footerContainer.appendChild(footerText);
+  return footerContainer;
+};
+
+export default createFooter;
